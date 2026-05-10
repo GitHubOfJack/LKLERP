8f5fbf367f820701f7113f645069c807c1b6784a
zhao.ma <zhao.ma@brgroup.com>
2026-05-10 15:30:38 +0800

    Cursor: Apply local changes for cloud agent

---
+++ backend/apps/holiday_calendar/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class HolidayCalendarConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.holiday_calendar'
+    verbose_name = '节假日日历'
